7688626f5a8d52e93fe4cde66d4874fe6b794ba3
munlikaCh <munlika_ch@cmu.ac.th>
2021-10-27 03:10:46 +0700

    link page

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
-import About from '../views/About.vue'
+
 import EventDetails from '@/views/event/Details.vue'
 import EventRegister from '@/views/event/Register.vue'
 import EventEdit from '@/views/event/Edit.vue'
+import Doccom from '@/views/event/Doccom.vue'
+
 import EventLayout from '@/views/event/Layout.vue'
-import NotFound from '@/views/NotFound.vue'
 import AddEvent from '@/views/EventForm.vue'
+
+import NotFound from '@/views/NotFound.vue'
 import NetWorkError from '@/views/NetworkError.vue'
 import NProgress from 'nprogress'
-import EventService from '@/services/EventService.js'
 import GStore from '@/store'
+
+import EventService from '@/services/EventService.js'
 import OrganizerService from '@/services/OrganizerService.js'
+// import VaccineService from "@/service/VaccineService";
+
+
 import Login from '@/views/LoginForm.vue'
 import Registration from '@/views/Registration.vue'
+//add
+import About from '../views/About.vue'
+import Start from "../views/Start.vue"
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'EventList',
     component: EventList,
-    props: (route) => ({ page: parseInt(route.query.page) || 1 })
+    props: (route) => ({ 
+      page: parseInt(route.query.page) || 1,
+      limit: parseInt(route.query.limit) || 4 
+    }),
   },
   {
     path: '/about',
     name: 'About',
     component: About
   },
+  {
+    path: "/",
+    name: "Start",
+    component: Start,
+  },
+  // หน้าโผล่
   {
     path: '/add-event',
     name: 'AddEvent',
@@ -71,18 +90,44 @@ const routes = [
         name: 'EventDetails',
         component: EventDetails
       },
+      //howetown
       {
         path: 'register',
         name: 'EventRegister',
         props: true,
         component: EventRegister
       },
+      //status
       {
         path: 'edit',
         name: 'EventEdit',
         props: true,
         component: EventEdit
-      }
+      },
+      //Doccom
+      {
+        path: "/doccom",
+        name: "Doccom",
+        // props: true,
+        component: Doccom,
+        beforeEnter: () => {
+          return OrganizerService.getOrganizers()
+            .then((response) => {
+              GStore.organizers = response.data
+            })
+            .catch(() => {
+              GStore.organizers = null
+              console.log('cannot load organizer')
+            })
+        }
+        // beforeEnter:(to) => {
+        //   if(GlobalState.currentUser.authorities[0] != "ROLE_ADMIN"){
+        //     return {
+        //       name: 'EventDetails'
+        //     }
+        //   }
+        // }
+      },
     ]
   },
   {
@@ -123,6 +168,9 @@ const router = createRouter({
       return { top: 0 }
     }
   }
+  //scrollBehavior(){
+  //  window.scrollTo(0,0);
+  //}
 })
 router.beforeEach(() => {
   NProgress.start()
@@ -131,4 +179,5 @@ router.beforeEach(() => {
 router.afterEach(() => {
   NProgress.done()
 })
+
 export default router
